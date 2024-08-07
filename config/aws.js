10c11d000a318ab28495e140a20080ae13c73e73
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-08-07 22:47:33 +0900

    Test: rollback test

--- config/aws.js
+++ config/aws.js
@@ -3,10 +3,11 @@ import AWS from "aws-sdk";
 // dotenv.config();
 
 //aws region 및 자격증명 설정
+// test
 AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,  
-    region: process.env.AWS_REGION,
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	region: process.env.AWS_REGION,
 });
 
 export default AWS;
